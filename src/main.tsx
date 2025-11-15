95609473f07b5727b3d62f42656f6851f2ab9132
hht <hht@163.com>
2025-11-16 02:48:41 +0800

    feat: 整合TDesign组件库

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
 import { RouterProvider, createRouter } from "@tanstack/react-router"
+import "tdesign-react/es/_util/react-19-adapter"
+import "tdesign-react/es/style/index.css"
 import "./app.css"
 
 // Import the generated route tree
